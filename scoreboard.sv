d83afcdcd470658ae9c99e113a6cc094cf7ad9d6
ModaDola <64921991+ModaDola@users.noreply.github.com>
2023-06-22 02:22:50 +0300

    Add hburst cases to scoreboard

--- scoreboard.sv
+++ scoreboard.sv
@@ -13,17 +13,47 @@ class scoreboard;
   endfunction
   
   //Compares the Actual result with the expected result
+  // ASSUMPTION: first cycle has htrans = IDLE
   task main;
-    transaction trans;
+    transaction trans, prev;
+    prev.hsel <= 0;
+    prev.haddr <= 0;
+    prev.hwrite <= 0;
+    hsize <= 0;
+    prev.hprot <= 0;
+    prev.htrans <= 0;
+    prev.hmastlock <= 0;
+    prev.hready <= 1'b1;
+    prev.hwdata <= 0;
+    prev.hburst <= 0;
+    prev.hreadyout <= 1'b1;
+    prev.hresp <= 0;
+    prev.hrdata <= 0;
+
     forever begin
       mon2scb.get(trans);
-        if((trans.a+trans.b) == trans.c)
-          $display("Result is as Expected");
-        else
-          $error("Wrong Result.\n\tExpeced: %0d Actual: %0d",(trans.a+trans.b),trans.c);
-        no_transactions++;
+      if (trans.hrans != 2'b00 && trans.htrans != 2'b01) begin // Do nothing if Transcation is IDLE or BUSY
+        case (trans.hburst)
+          3'b000: // Single transfer
+          3'b001: // INCR
+          3'b010: // WRAP4
+          3'b011: // INCR4
+          3'b100: // WRAP8
+          3'b101: // INCR8
+          3'b110: // WRAP16
+          3'b111: // INCR16
+          default: ;
+        endcase
+      end;
+      no_transactions++;
       trans.display("[ Scoreboard ]");
+      prev = trans;
     end
   endtask
+
+  function single_transfer(transaction prev, transaction trans);
+    ;
+    
+  endfunction
   
 endclass
